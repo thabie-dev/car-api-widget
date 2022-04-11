b5c7b11b283e40ed0f19094338a545975f1df076
thabie-dev <taniatdube1@gmail.com>
2022-04-11 18:54:33 +0200

    carwidget

--- index.js
+++ index.js
@@ -1,40 +1,100 @@
-const carsElem = document.querySelector('.cars');
+const carsElem = document.querySelector('.carsElem');
 const color = document.querySelector('.color')
 const button = document.querySelector('.button')
 const brand = document.querySelector('.brand')
+const make = document.querySelector('.make')
 
-button.addEventListener('click', function (){
+
+
+
+button.addEventListener('click', function () {
     console.log('color')
-    console.log(color.value)
+    console.log("color", color.value)
     console.log(brand.value)
-    const url= `https://api-tutor.herokuapp.com/v1/cars/make/${brand.value}/color/${color.value}`
+    if (color.value != "" && brand.value != "") {
+        const url = `https://api-tutor.herokuapp.com/v1/cars/make/${brand.value}/color/${color.value}`
 
-    axios.get(url).then(function(result){
-        console.log(result.data);
-    
-        result.data.forEach(car => {
-            const tableElement = document.createElement('tr');
-            
-            tableElement.innerHTML = `<tr>
+
+        axios.get(url).then(function (result) {
+            console.log(result.data);
+
+            result.data.forEach(car => {
+                const tableElement = document.createElement('tr');
+
+                tableElement.innerHTML = `<tr>
+                    <td>${car.model}</td>
+                    <td>${car.color}</td>
+                    <td>${car.make}</td>
+                </tr>`
+                carsElem.appendChild(tableElement);
+
+
+            });
+        })
+
+    }
+
+    else if (color.value == "" && brand.value != "") {
+        const url = `https://api-tutor.herokuapp.com/v1/cars/make/${brand.value}`
+        axios.get(url).then(function (result) {
+            console.log(result.data);
+
+            result.data.forEach(car => {
+                const tableElement = document.createElement('tr');
+
+                tableElement.innerHTML = `<tr>
                 <td>${car.model}</td>
                 <td>${car.color}</td>
                 <td>${car.make}</td>
             </tr>`
-        carsElem.appendChild(tableElement);
-    
-        });
-    })
-    
-});
+                carsElem.appendChild(tableElement);
 
-// document.addEventListener('DOMContentLoaded',() => {
 
-    // const selectcar = document.getElementById('cars');
+            });
+        })
 
-    // fetch('https://github.com/avermeulen/api-tutor').then(res => {
-        // return res.json();
-    // }).then(data =>{
-// 
-    // })
-    
-// });
+
+    }
+
+    else if (color.value != "" && brand.value == "") {
+        const url = `https://api-tutor.herokuapp.com/v1/cars/color/${color.value}`
+        axios.get(url).then(function (result) {
+            console.log(result.data);
+
+            result.data.forEach(car => {
+                const tableElement = document.createElement('tr');
+
+                tableElement.innerHTML = `<tr>
+                <td>${car.model}</td>
+                <td>${car.color}</td>
+                <td>${car.make}</td>
+            </tr>`
+                carsElem.appendChild(tableElement);
+
+
+            });
+        })
+
+
+    }
+    else {
+        const url = `https://api-tutor.herokuapp.com/v1/cars/`
+        axios.get(url).then(function (result) {
+            console.log(result.data);
+
+            result.data.forEach(car => {
+                const tableElement = document.createElement('tr');
+
+                tableElement.innerHTML = `<tr>
+                <td>${car.model}</td>
+                <td>${car.color}</td>
+                <td>${car.make}</td>
+            </tr>`
+                carsElem.appendChild(tableElement);
+
+
+            });
+        })
+
+    }
+})
